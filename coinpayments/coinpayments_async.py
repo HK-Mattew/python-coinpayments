4b404fba7e1589e9f90f0ebb835ece066b7fef5d
HK-Mattew <mattewsub6565@gmail.com>
2024-06-02 12:41:23 -0300

    Fixes + Improvements.

--- coinpayments/coinpayments_async.py
+++ coinpayments/coinpayments_async.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlencode
+from .helpers.encoders import http_data_encoder_for_php
 from typing import Literal
 import hashlib
 import httpx
@@ -18,15 +18,18 @@ class CoinPaymentsAsyncIO():
     async def __gen_hmac(self, data: dict) -> str:
         return hmac.new(
             bytes(self.__PRIVATE_KEY, 'utf-8'),
-            urlencode(data).encode('utf-8'),
+            http_data_encoder_for_php(data).encode('utf-8'),
             hashlib.sha512
         ).hexdigest()
 
 
     async def __request_api(self, data: dict, method: Literal['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS']):
 
+        if not 'cmd' in data:
+            raise ValueError('The field `cmd` is mandatory when requesting the API')
+
+
         data.update({
-            'cmd': data.get('cmd'),
             'key': self.__PUBLIC_KEY,
             'version': self.__VERSION,
             'format': 'json'
@@ -34,11 +37,22 @@ class CoinPaymentsAsyncIO():
 
         hmac_signature = await self.__gen_hmac(data)
 
+        content = http_data_encoder_for_php(data).encode('utf-8')
+
+        content_length = str(len(content))
+        content_type = "application/x-www-form-urlencoded"
+
+        headers = {
+            'Content-Length': content_length,
+            'Content-Type': content_type,
+            'hmac': hmac_signature
+            }
+
         async with httpx.AsyncClient() as client:
             response = await getattr(client, method.lower())(
                 url=self.__BASE_ENDPOINT,
-                data=data,
-                headers={'hmac': hmac_signature}
+                content=content,
+                headers=headers
             )
 
             return response.json()
